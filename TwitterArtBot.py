3aec5a748f6fee1ec035e50f747e0774341e90cf
Joe Stauss <jstauss008@gmail.com>
2021-01-02 09:54:27 -0800

    Delete TwitterArtBot.py

--- TwitterArtBot.py
+++
@@ -1,5 +0,0 @@
-from browser_automation import *
-
-if __name__ == "__main__":
-    artbot = TwitterArtBot(  "TaglinesOTerror", "***REMOVED***")
-    artbot.text_post( "You've never been scared until you've been scared in 3-D. #HouseOfWax")
