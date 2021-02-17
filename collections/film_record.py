8721e1df5b258b917579acecaee5a857d532b230
Joe Stauss <jstauss008@gmail.com>
2021-02-16 19:32:54 -0800

    FilmRecord is JSONableMapping
    * FilmRecord is now a JSONableMapping, which took the kludges out of FilmRecord and FilmSet classes.

--- collections/film_record.py
+++ collections/film_record.py
@@ -1,16 +1,4 @@
-from py_util.collections import BaseMapping
-import collections.abc
+from py_util.collections import BaseMapping, JSONableMapping
 
-class FilmRecord( collections.abc.Hashable, BaseMapping):
-    global_counter = 0
-
-    def __init__( self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._hash = FilmRecord.global_counter
-        FilmRecord.global_counter = FilmRecord.global_counter + 1
-
-    def __hash__( self):
-        return self._hash
-
-    def __eq__( self, other):
-        return self.data == other.data
+class FilmRecord( BaseMapping, JSONableMapping):
+    pass
--- collections/film_set.py
+++ collections/film_set.py
@@ -1,6 +1,4 @@
 from py_util.collections import BaseSet
 
 class FilmSet( BaseSet):
-    def __eq__( self, other):
-        print( [obj in other.data for obj in self.data])
-        return all( obj in other.data for obj in self.data)
+    pass
